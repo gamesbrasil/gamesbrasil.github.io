c0806a531cb2abb472fbd0e4d742147c6c27ba2a
José Espinheira <joseespinheira@gmail.com>
2019-11-06 21:38:14 -0300

    Criado menu

---
+++ menu.js
@@ -0,0 +1,18 @@
+
+var menuGeral = "";
+menuGeral += '<ul>';
+menuGeral += '<li> <a href="index.html">Página Inicial</a> </li>';
+menuGeral += '<li> <a href="jogoGato.html">Jogo do Gato</a> </li>';
+menuGeral += '<li> <a href="jogoVelha.html">Jogo da Velha</a> </li>';
+menuGeral += '</ul>';
+// menuGeral += '<ul id="menuGeral">    <li><a href="jogoGato.html">Jogo do Gato</a></li>    <li><a href="jogoVelha.html">Jogo da Velha</a></li>    </ul>';
+// menuGeral += '<ul id="menuGeral">    <li><a href="jogoGato.html">Jogo do Gato</a></li>    <li><a href="jogoVelha.html">Jogo da Velha</a></li>    </ul>';
+// menuGeral += '<ul id="menuGeral">    <li><a href="jogoGato.html">Jogo do Gato</a></li>    <li><a href="jogoVelha.html">Jogo da Velha</a></li>    </ul>';
+// menuGeral += '<ul id="menuGeral">    <li><a href="jogoGato.html">Jogo do Gato</a></li>    <li><a href="jogoVelha.html">Jogo da Velha</a></li>    </ul>';
+// menuGeral += '<ul id="menuGeral">    <li><a href="jogoGato.html">Jogo do Gato</a></li>    <li><a href="jogoVelha.html">Jogo da Velha</a></li>    </ul>';
+// menuGeral += '<ul id="menuGeral">    <li><a href="jogoGato.html">Jogo do Gato</a></li>    <li><a href="jogoVelha.html">Jogo da Velha</a></li>    </ul>';
+
+var footer = "";
+footer +='<div>';
+
+footer +='</div>'
