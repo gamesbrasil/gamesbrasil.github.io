7e828c399d602673725df53770119e5ecd1ba338
José Espinheira <joseespinheira@gmail.com>
2020-03-01 22:39:22 -0300

    Organizando diretorios

--- menu.js
+++ menu.js
@@ -2,8 +2,8 @@
 var menuGeral = "";
 menuGeral += '<ul>';
 menuGeral += '<li> <a href="index.html">Página Inicial</a> </li>';
-menuGeral += '<li> <a href="jogoGato.html">Jogo do Gato</a> </li>';
-menuGeral += '<li> <a href="jogoVelha.html">Jogo da Velha</a> </li>';
+menuGeral += '<li> <a href="JOGO_GATO/jogoGato.html">Jogo do Gato</a> </li>';
+menuGeral += '<li> <a href="JOGO_VELHA/jogoVelha.html">Jogo da Velha</a> </li>';
 menuGeral += '</ul>';
 // menuGeral += '<ul id="menuGeral">    <li><a href="jogoGato.html">Jogo do Gato</a></li>    <li><a href="jogoVelha.html">Jogo da Velha</a></li>    </ul>';
 // menuGeral += '<ul id="menuGeral">    <li><a href="jogoGato.html">Jogo do Gato</a></li>    <li><a href="jogoVelha.html">Jogo da Velha</a></li>    </ul>';
